edc61ddb3660d56fe2bc2f64983e02cbce370e32
fuzailnizmani <fuzailnizamani13@gmail.com>
2025-02-16 17:49:32 +0500

    Add signup logic in different file

--- routes/Signup.js
+++ routes/Signup.js
@@ -1,24 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+const sigup = require('../controllers/signupController');
 
-router.post('/', async (req, res) => {
-  try {
-    const { username, pwd } = req.body;
-    if (!username || !pwd) {
-      res.status(404).json({ message: ' username and password is requested' });
-    }
-    const hashedpassword = await bcrypt.hash(pwd, 10);
-    const user = new User({
-      username: username,
-      password: hashedpassword
-    });
-    await user.save();
-    res.status(201).json({ message: 'User registered successfully ' });
-  } catch (error) {
-    res.status(500).json({ error: 'server failures' });
-  }
-});
+router.post('/', sigup);
 
 module.exports = router;
