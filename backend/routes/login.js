76710e4f2f0ff7aadeefd56638fbc44c61b5c957
fuzailnizmani <fuzailnizamani13@gmail.com>
2025-02-18 21:01:56 +0500

    create different folder for backend and frontend

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require('../controllers/loginController');
+const login = require('../backend/controllers/loginController');
 
 router.post('/', login);
 
